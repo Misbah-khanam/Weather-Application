67a7678fbf16d9010fdf13bd98863fc0af67e32d
Misbah-khanam <misbahkhanam711@gmail.com>
2023-04-29 12:57:22 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Search from './Search.js';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Search/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Search.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import api from './api/axiosConfig.js'
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+import './Search.css';
+
+const Search = () => {
+const [weather, setWeather] = useState(" ");
+  const [temperature, setTemperature] = useState(0);
+  const [humidity, setHumidity] = useState(0);
+  const [windSpeed, setWindSpeed] = useState(0);
+  const [pressure, setPressure] = useState(0);
+  const [location, setLocation] = useState(" ");
+
+  const getData = async() => {
+    try {
+      const response = await api.get(`/api/v1/weather?city=${location}`);
+      console.log(response.data.weather[0].description);
+      setWeather(response.data.weather[0].description);
+      setTemperature(response.data.main.temp);
+      setHumidity(response.data.main.humidity);
+      setWindSpeed(response.data.wind.speed);
+      setPressure(response.data.main.pressure);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  return (
+    <div>
+        <div className='heading-container'>
+            <h1 className='heading'>Current Weather Details</h1>
+        </div>
+        <div>
+            <input placeholder='location' onChange={(e) => {setLocation(e.target.value)}} className='search-bar'/>
+            <button type='submit' onClick={getData} className='search-btn'>Search</button>
+        </div>
+        <div className='weather-div'>
+            <h2>Weather Details</h2>
+            <table className='weather-table'>
+                <tbody>
+                <tr className='weather-row'>
+                    <td className='weather-data1'>weather</td>
+                    <td className='weather-data'>{weather}</td>
+                </tr>
+                <tr className='weather-row'>
+                    <td className='weather-data1'>temperature</td>
+                    <td className='weather-data'>{temperature}</td>
+                </tr>
+                <tr className='weather-row'>
+                    <td className='weather-data1'>humidity</td>
+                    <td className='weather-data'>{humidity}</td>
+                </tr>
+                <tr className='weather-row'>
+                    <td className='weather-data1'>windSpeed</td>
+                    <td className='weather-data'>{windSpeed}</td>
+                </tr>
+                <tr className='weather-row'>
+                    <td className='weather-data1'>pressure</td>
+                    <td className='weather-data'>{pressure}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+  )
+}
+
+export default Search
